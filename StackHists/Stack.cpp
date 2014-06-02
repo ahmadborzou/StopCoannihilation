b597f007d16f2faf1efbd3165b464032ccaab50d
ahmad <ahmad_borzou@baylor.edu>
2014-06-02 13:20:06 -0500

    :)

--- StackHists/Stack.cpp
+++ StackHists/Stack.cpp
@@ -38,6 +38,7 @@ map<string, map<string , vector<TH1D> > > map_map; //here the string shows the t
 vector< map<string, map<string , vector<TH1D> > >  > vec_map_map; //this is a vector of previous map. Each component of the vector corresponds to one HTbin. 
                                                                   //In BJ case there are 7 of them.
 TFile *file;
+vector<TFile *> inputfilevec;
 TH1D *temphist;
 //KKH TH1D temphist;
 vector<TH1D > vtemphist;
@@ -138,13 +139,50 @@ histname[17]="NTau";
 //const int nHT = 1;
   const int nHist = 18; // Number of histograms in each TDirectory
 //const int nHist = 5;
+for(int i=1; i<=nHT ; i++){
+sprintf(tempname,"../Results/results_PhaseI_BJ_14TEV_HT%d_NoPileUp.root",i);
+inputfilevec.push_back(TFile::Open(tempname,"R"));
+}
+
+
+
+
+tempstack = new THStack("stack","Binned Sample Stack");
+file = new TFile("stack.root","RECREATE");
+ for(map<int , string >::iterator itt=type.begin(); itt!=type.end();itt++){        // loop over different event types
+    cdtoitt = file->mkdir((itt->second).c_str());
+    cdtoitt->cd();
+    for(map<int , string >::iterator it=cutname.begin(); it!=cutname.end();it++){   // loop over different cutnames
+      cdtoit =  cdtoitt->mkdir((it->second).c_str());
+      cdtoit->cd();
+      for(int j=0; j<histname.size(); j++){                                        // loop over different histograms
+        for(int i=0; i<nHT ; i++){                                                  // loop over different HT bins
+sprintf(tempname,"%s/%s/%s_%s_%s",(itt->second).c_str(),(it->second).c_str(),(histname[j]).c_str(),(it->second).c_str(),(itt->second).c_str());
+temphist = (TH1D *) inputfilevec.at(i)->Get(tempname)->Clone();
+          tempstack->Add(temphist);
+               }//end of loop over HTbins 1..7
+        sprintf(tempname,"%s_%s_%s",histname[j].c_str(),(it->second).c_str(),(itt->second).c_str());
+        tempstack->Write(tempname);
+        delete tempstack;
+        tempstack = new THStack("stack","Binned Sample Stack");
+      }//end of loop over j=0..3
+    }//end of loop over map
+  }//end of loop over map
+file->Close();
+
+
+
+
+
+/*
+
+
+
   //load the histograms into the vec_map_map  
   for(int i=1; i<=nHT ; i++){    // loop over different HT bins
     map_map.clear();
     //open the file
-    sprintf(tempname,"../Results/results_PhaseI_BJ_14TEV_HT%d_NoPileUp.root",i);
-    file = new TFile(tempname, "R");
-if(file->IsOpen()){cout << "The file " << tempname << "  opened"<< endl; }else{cout << " The source file couldn't be opened" << endl;}
+if(inputfilevec.at(i-1)->IsOpen()){cout << "The " << i << "th file has been opened"<< endl; }else{cout << " The source file couldn't be opened" << endl;}
     for(map<int , string >::iterator itt=type.begin(); itt!=type.end();itt++){       // loop over different event types
       for(map<int , string >::iterator it=cutname.begin(); it!=cutname.end();it++){  // loop over diffrent cut names
 	temphistvec.clear();
@@ -156,7 +194,7 @@ if(file->IsOpen()){cout << "The file " << tempname << "  opened"<< endl; }else{c
 	  sprintf(newhistname,"%s/%s/%s_%s_%s_HT%d",(itt->second).c_str(),(it->second).c_str(),
 		  (ithist->second).c_str(),(it->second).c_str(),(itt->second).c_str(),i);
 	  //sprintf(tempname,"allEvents/RA2nocut/allEvents_RA2nocut_hist%d",j);
-	  vtemphist.push_back(*(TH1D*) file->Get(tempname));
+	  vtemphist.push_back(*(TH1D*) inputfilevec.at(i-1)->Get(tempname));
 	  vtemphist.back().SetName(newhistname);//scale the hists before loading them 
 	  vtemphist.back().SetTitle(newhistname);//scale the hists before loading them 
 	  vtemphist.back().Scale(scalevec[i-1]);//scale the hists before loading them 
@@ -186,8 +224,8 @@ cout << " +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << en
     vec_map_map.push_back(map_map);                     // vector of histograms - 
   }//end of loop over HTbins 1..7
 //  std::cout << "loading histograms done" << std::endl;
-
-  tempstack = new THStack("stack","Binned Sample Stack");
+*/
+/* tempstack = new THStack("stack","Binned Sample Stack");
  
   file = new TFile("stack.root","RECREATE");
 
@@ -227,7 +265,7 @@ if(file->IsOpen()){cout << "stack.root has been opened to be written! Wooaaa" <<
       }//end of loop over j=0..3
     }//end of loop over map
   }//end of loop over map
-file->Close();
+file->Close();*/
 }//end of the constructor
 };
 
@@ -236,3 +274,4 @@ mainClass mainObj(3000000);
 cout << " done :) " << endl;
 
 }
+
